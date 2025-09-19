357fc0a2d9a4a8afb485588a748f88287768ab1d
lucascamourao <lucascamourao@gmail.com>
2025-09-18 21:20:44 -0300

    more quotes

--- quotes.js
+++ quotes.js
@@ -3,5 +3,15 @@ export const quotes = [
   "Do not wait to strike till the iron is hot, but make it hot by striking. – William Butler Yeats",
   "In the middle of every difficulty lies opportunity. – Albert Einstein",
   "It always seems impossible until it’s done. – Nelson Mandela",
-  "Simplicity is the soul of efficiency. – Austin Freeman"
+  "Simplicity is the soul of efficiency. – Austin Freeman",
+  "Success usually comes to those who are too busy to be looking for it. – Henry David Thoreau",
+  "Don’t watch the clock; do what it does. Keep going. – Sam Levenson",
+  "Opportunities don't happen, you create them. – Chris Grosser",
+  "Act as if what you do makes a difference. It does. – William James",
+  "Quality is not an act, it is a habit. – Aristotle",
+  "Hard work beats talent when talent doesn’t work hard. – Tim Notke",
+  "The only way to do great work is to love what you do. – Steve Jobs",
+  "If you can dream it, you can achieve it. – Zig Ziglar",
+  "Start where you are. Use what you have. Do what you can. – Arthur Ashe",
+  "Perseverance is not a long race; it is many short races one after the other. – Walter Elliot"
 ];
